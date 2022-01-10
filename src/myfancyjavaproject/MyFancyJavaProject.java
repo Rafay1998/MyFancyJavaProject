a77c80f07a27d3d86f5492bd42395b5879518930
Rafay1998 <szrafay.@gmail.com>
2022-01-10 21:03:04 +0600

    My first commit

---
+++ src/myfancyjavaproject/MyFancyJavaProject.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfancyjavaproject;
+
+/**
+ *
+ * @author MD Samiuzzaman Rafay
+ */
+public class MyFancyJavaProject {
+    public static void main(String[] args) {
+
+      Shape circle = new Circle();
+      System.out.println("Circle with normal border");
+      circle.draw();
+   }
+}
